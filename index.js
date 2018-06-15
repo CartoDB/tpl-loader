ba2bc6a8850bd2cced169485557506f65e6ffea6
rubenmoya <ruben@carto.com>
2018-06-15 15:12:43 +0200

    Add default value to options

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var _ = require('lodash');
 module.exports = function (source) {
   this.cacheable && this.cacheable();
 
-  var options = loaderUtils.getOptions(this);
+  var options = loaderUtils.getOptions(this) || {};
   var template = _.template(source, null, options.tplSettings);
 
   return 'var _ = require(\'underscore\');\nmodule.exports = ' + template;
