1e0d055cca80096136656cf61e4d00ee9b30d23a
rubenmoya <ruben@carto.com>
2018-06-15 09:20:04 +0200

    Update for webpack 4

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+var loaderUtils = require('loader-utils');
 var _ = require('lodash');
 
 module.exports = function (source) {
   this.cacheable && this.cacheable();
-  var template = _.template(source, null, this.options.tplSettings);
+
+  var options = loaderUtils.getOptions(this);
+  var template = _.template(source, null, options.tplSettings);
+
   return 'var _ = require(\'underscore\');\nmodule.exports = ' + template;
 };
